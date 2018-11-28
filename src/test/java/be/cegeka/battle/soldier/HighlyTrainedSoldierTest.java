0f6abbea3fd1a19a7ef8542a743c4d73aa48d12f
dumitruc <dumitru.cernei@cegeka.com>
2018-11-28 15:35:11 +0200

    Highly specialized soldier

---
+++ src/test/java/be/cegeka/battle/soldier/HighlyTrainedSoldierTest.java
@@ -0,0 +1,15 @@
+package be.cegeka.battle.soldier;
+
+import org.junit.Test;
+
+/**
+ * Created by dumitruc on 28.11.2018.
+ */
+public class HighlyTrainedSoldierTest {
+
+    @Test
+    public void givenAttackerWithPotionAndEnemyWithNoPation_whenAttacking_thenCheckPotionEffect() {
+        Soldier soldier1 = new HighlyTrainedSoldier();
+    }
+
+}
