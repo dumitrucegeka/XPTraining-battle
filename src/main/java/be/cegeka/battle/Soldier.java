4ed21b92d2bce27173a0ac03981fa4c10c1a9bc6
dumitruc <dumitru.cernei@cegeka.com>
2018-11-28 14:08:33 +0200

    Add Weapon interface

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -1,5 +1,6 @@
 package be.cegeka.battle;
 
+import be.cegeka.battle.weapons.Weapon;
 import org.apache.commons.lang3.Validate;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
@@ -7,11 +8,25 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 public class Soldier {
 
     private final String name;
+    private Weapon weapon;
 
     public Soldier(String name) {
         Validate.isTrue(isNotBlank(name));
 
         this.name =  name;
+    }
+
+    public Soldier(String name, Weapon weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    public int attackPower() {
+        return weapon.getDamage();
+    }
+
+    public boolean attack(Soldier soldier) {
+        return attackPower() >= soldier.attackPower();
     }
 
     String getName() {
