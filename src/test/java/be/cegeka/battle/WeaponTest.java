402afc3d5cffd0730d64a3967dc2406726178b53
stefancernescu <stefan.cernescu@cegeka.com>
2018-11-28 13:58:11 +0200

    added test for weapon

---
+++ src/test/java/be/cegeka/battle/WeaponTest.java
@@ -0,0 +1,14 @@
+package be.cegeka.battle;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WeaponTest {
+
+    @Test
+    public void givenSwordDamageIsCorrect() {
+
+    }
+}
